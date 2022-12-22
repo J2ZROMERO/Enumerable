4720ce88bbad3b707065f2ddfca966bc3ae34d54
lana <diegogarciaq98@gmail.com>
2022-12-22 12:18:27 -0500

    Update mylist

--- MyList.rb
+++ MyList.rb
@@ -1,28 +1,19 @@
-require_relative 'MyEnumerable'
- class MyList
-include MyEnumerable
+require_relative 'myEnumerable'
 
-    def initialize (list)
-      @list = list
-    end
-  
-    # filter?
-    # any?
-    def each
-        
-        
-        if block_given?
-        return     yield(@list)
-        else
-            puts "no  block"
-
-        end
-        
-    end 
+class MyList
+  include MyEnumerable
+  def initialize (list)
+    @list = list
   end
-  
-  list = MyList.new([1, 2, 3, 4])
-  
-  list.each { |x| puts x }
-  
-  
+
+  def each
+    yield(@list)
+  end 
+end
+
+list = MyList.new([1, 2, 3, 4] )
+puts(list.all? { |e| e < 5} )
+puts(list.all? { |e| e > 5} )
+puts(list.any? { |e| e == 2} )
+puts(list.any? { |e| e == 5} )
+puts(list.filter { |e| e.even?} )
