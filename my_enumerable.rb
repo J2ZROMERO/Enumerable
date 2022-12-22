c59d0e348fe1dc733163cb8b261d6971fe3a6a5d
Jhosep <jose-zromero@hotmail.com>
2022-12-22 11:32:52 -0600

    Apply rubocop suggestions

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,18 +1,17 @@
 module MyEnumerable
-    def all?
-      @list.each { |item| return false unless yield(item) }
-      true
-    end
-  
-    def any?
-      @list.each { |item| return true if yield(item) }
-      false
-    end
-  
-    def filter
-      arr = []
-      @list.each { |item| arr.push(item) if yield(item) }
-      arr
-    end
+  def all?
+    @list.each { |item| return false unless yield(item) }
+    true
   end
-  
+
+  def any?
+    @list.each { |item| return true if yield(item) }
+    false
+  end
+
+  def filter
+    arr = []
+    @list.each { |item| arr.push(item) if yield(item) }
+    arr
+  end
+end
