9c7f433a7dc7d09b75164b8062e9a43bcd66aa4e
Jhosep <jose-zromero@hotmail.com>
2022-12-22 11:32:17 -0600

    add features

---
+++ my_list.rb
@@ -0,0 +1,20 @@
+require_relative 'my_enumerable'
+
+class MyList
+  include MyEnumerable
+
+  def initialize(list)
+    @list = list
+  end
+
+  def each
+    yield(@list)
+  end
+end
+
+list = MyList.new([1, 2, 3, 4])
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
+puts(list.filter(&:even?))
